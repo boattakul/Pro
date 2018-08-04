2757b6feea3ea7590f0c068aa51cc28318a65a8d
web-pm <boat.nattapong@hotmail.com>
2018-08-04 21:08:59 +0700

    fist
    fistcc

---
+++ src/app/shareds/services/account.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+import { IRegister } from '../../components/register/register.interface';
+@Injectable()
+export class AccountService {
+
+    // ลงทะเบียน
+    onRegister(model: IRegister) {
+        return new Promise((resolve, reject) => {
+            resolve(model);
+        });
+    }
+
+}
